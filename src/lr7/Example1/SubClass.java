343118623a2fcaab96a8995a33b11012f3dadb32
HoneyRinn <fyodorov.ilja2017@yandex.ru>
2024-05-06 12:14:23 +0500

    lr7 (1,2)

---
+++ src/lr7/Example1/SubClass.java
@@ -0,0 +1,22 @@
+package lr7.Example1;
+
+public class SubClass extends SuperClass{
+    private String SubCField;
+
+    SubClass(String text){
+        super(text);
+    }
+
+    SubClass(String text1, String text2){
+        super(text1);
+        this.SubCField = text2;
+    }
+
+    @Override
+    public String toString(){
+        String label = "Class name = " + this.getClass().getSimpleName() + "\n" +
+                "textField1 = " + this.getField() + "\n" +
+                "textField2 = " + this.SubCField;
+        return  label;
+    }
+}
